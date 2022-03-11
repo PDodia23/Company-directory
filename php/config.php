ce957ea735a5e8dacb31c277b2483453e792f1e8
PoojaDodia <65901814+PDodia23@users.noreply.github.com>
2022-03-11 16:10:16 +0000

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "db5006818024.hosting-data.io";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "dbs5629465";
+	$cd_user = "dbu1219230";
+	$cd_password = "Eeyore230890";
+
+?>
